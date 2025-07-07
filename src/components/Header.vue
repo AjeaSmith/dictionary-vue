93cea3d86ab1bd1ffddc538bd0566085c4322be2
ajea <smithajea@rocketmail.com>
2025-07-07 18:39:36 -0400

    Switch between serif, sans serif, and monospace fonts

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,10 @@
 <template>
   <header>
     <IconLogo />
-    <div>
+    <div class="right-side">
       <!-- dropdown fonts -->
+      <FontSelect />
+      <div class="divider" />
       <!-- theme switcher -->
       <ThemeSwitcher />
     </div>
@@ -10,6 +12,7 @@
 </template>
 
 <script setup>
+import FontSelect from "./FontSelect.vue";
 import IconLogo from "./icons/IconLogo.vue";
 import ThemeSwitcher from "./ThemeSwitcher.vue";
 </script>
@@ -19,5 +22,15 @@ header {
   display: flex;
   align-items: center;
   justify-content: space-between;
+}
+.right-side {
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+}
+.divider {
+  width: 1px;
+  height: 32px;
+  background-color: var(--separator);
 }
 </style>
