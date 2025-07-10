42f2e69b3caf70c88b190f48979586279b3f9a76
ajea <smithajea@rocketmail.com>
2025-07-10 14:02:18 -0400

    remove test watch effect

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { watch } from "vue";
 import Header from "./components/Header.vue";
 import NotFound from "./components/NotFound.vue";
 import SearchBar from "./components/SearchBar.vue";
@@ -7,10 +6,6 @@ import SearchResults from "./components/SearchResults.vue";
 import { useSearch } from "./composables/useSearch";
 
 const { notFound, isLoading } = useSearch();
-
-watch(notFound, (newValue) => {
-  console.log(newValue);
-});
 </script>
 
 <template>
